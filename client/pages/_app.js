60e0c6257e1840eff7c6b3c68eaddc3f34e32230
Hans141 <hans@techainer.com>
2022-01-20 10:32:47 +0700

    add fb messenger

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -15,6 +15,30 @@ import { useStore } from "../store";
 // export default MyApp
 
 export default function App({ Component, pageProps }) {
+  var messenger = `
+  <!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+  window.fbAsyncInit = function() {
+  FB.init({
+      xfbml            : true,
+      version          : 'v7.0'
+  });
+  };
+  (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+  attribution=setup_tool
+  page_id="103703514917752"
+  theme_color="#67b868">
+</div>
+`
   const store = useStore(pageProps.initialReduxState);
 
   return (
@@ -25,6 +49,10 @@ export default function App({ Component, pageProps }) {
     >
       <Provider store={store}>
         <Component {...pageProps} />
+        <div
+          className="content"
+          dangerouslySetInnerHTML={{ __html: messenger }}
+        ></div>
       </Provider>
     </ToastProvider>
   );
